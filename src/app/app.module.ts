98ff922546a97a59d99b17fd18dd4e637ed27348
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2019-11-21 12:15:13 +0100

    contract component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ContractComponent } from './contract/contract.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ContractComponent
   ],
   imports: [
     BrowserModule
