96c5ce0bed113bcaed86f50dc0f0d997fc5ac798
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2019-11-21 12:17:10 +0100

    contract-create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ContractComponent } from './contract/contract.component';
 import { ContractListComponent } from './contract-list/contract-list.component';
+import { ContractCreateComponent } from './contract-create/contract-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ContractComponent,
-    ContractListComponent
+    ContractListComponent,
+    ContractCreateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/contract-create/contract-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-create',
+  templateUrl: './contract-create.component.html',
+  styleUrls: ['./contract-create.component.css']
+})
+export class ContractCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
