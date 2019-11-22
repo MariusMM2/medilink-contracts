df2bb6490f63eec678890ac18a18da2896b3abac
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2019-11-22 15:13:47 +0100

    added contract entity

---
+++ src/app/entities/contract.ts
@@ -0,0 +1,19 @@
+export class Contract {
+  id: string;
+  name1: string;
+  description: string;
+  start: string;
+  expirationDate: string;
+  type: string;
+  file1: string;
+  // user: User;
+}
+
+// export class User {
+//   id: string;
+//   username: string;
+//   password?: string;
+//   email: string;
+//   gender: string;
+//   birthDate: Date;
+// }
