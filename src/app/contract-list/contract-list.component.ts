cfa07828b7dfc190e24aac8765fbc0e7982d8f55
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2019-11-29 15:50:19 +0100

    contracts table display

--- src/app/contract-list/contract-list.component.ts
+++ src/app/contract-list/contract-list.component.ts
@@ -4,13 +4,25 @@ import {ContractService} from '../services/contract.service';
 import {Contract} from '../entities/contract';
 import {Sort} from '@angular/material/sort';
 
-export interface Dessert {
-  calories: number;
-  carbs: number;
-  fat: number;
+export interface PeriodicElement {
   name: string;
-  protein: number;
+  position: number;
+  weight: number;
+  symbol: string;
 }
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-contract-list',
@@ -23,41 +35,11 @@ export class ContractListComponent implements OnInit {
   // userSearch: string;
   // isAdmin$: Observable<boolean>;
 
-  desserts: Dessert[] = [
-    {name: 'Frozen yogurt', calories: 159, fat: 6, carbs: 24, protein: 4},
-    {name: 'Ice cream sandwich', calories: 237, fat: 9, carbs: 37, protein: 4},
-    {name: 'Eclair', calories: 262, fat: 16, carbs: 24, protein: 6},
-    {name: 'Cupcake', calories: 305, fat: 4, carbs: 67, protein: 4},
-    {name: 'Gingerbread', calories: 356, fat: 16, carbs: 49, protein: 4},
-  ];
-
-  sortedData: Dessert[];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
   constructor( private contractService: ContractService) {
-
-    this.sortedData = this.desserts.slice();
   }
-
-  sortData(sort: Sort) {
-    const data = this.desserts.slice();
-    if (!sort.active || sort.direction === '') {
-      this.sortedData = data;
-      return;
-    }
-
-    this.sortedData = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'calories': return compare(a.calories, b.calories, isAsc);
-        case 'fat': return compare(a.fat, b.fat, isAsc);
-        case 'carbs': return compare(a.carbs, b.carbs, isAsc);
-        case 'protein': return compare(a.protein, b.protein, isAsc);
-        default: return 0;
-      }
-    });
-  }
-
 
   ngOnInit() {
     this.isLoading$ = new Observable(subscriber => {
@@ -70,9 +52,4 @@ export class ContractListComponent implements OnInit {
       }, 2000);
     });
   }
-}
-
-
-function compare(a: number | string, b: number | string, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
