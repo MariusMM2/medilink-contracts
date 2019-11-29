0734f0e1f9de98c971be58f28305e1b4e4761cfb
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2019-11-29 16:27:25 +0100

    contracts table - mock-up finished

--- src/app/contract-list/contract-list.component.ts
+++ src/app/contract-list/contract-list.component.ts
@@ -12,16 +12,17 @@ export interface PeriodicElement {
   endDate: string;
   type: string;
   file: string;
+  actions: string;
 
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Contract 1', description: 'desc 1', startDate: '29.NOV.2019', endDate: '30.NOV.2019', type: 'company to company', file: 'file1.pdf' },
-  {position: 2, name: 'Contract 2', description: 'desc 2', startDate: '12.JUN.2019', endDate: '05.DEC.2019', type: 'company to employee', file: 'file2.pdf' },
-  {position: 3, name: 'Contract 3', description: 'desc 3', startDate: '13.JUN.2019', endDate: '03.DEC.2019', type: 'company to company', file: 'file3.pdf' },
-  {position: 4, name: 'Contract 4', description: 'desc 4', startDate: '14.JUN.2019', endDate: '08.DEC.2019', type: 'company to employee', file: 'file4.pdf' },
-  {position: 5, name: 'Contract 5', description: 'desc 5', startDate: '15.JUN.2019', endDate: '06.DEC.2019', type: 'company to employee', file: 'file5.pdf' },
-  {position: 6, name: 'Contract 6', description: 'desc 6', startDate: '16.JUN.2019', endDate: '09.DEC.2019', type: 'company to company', file: 'file6.pdf' },
+  {position: 1, name: 'Contract 1', description: 'desc 1', startDate: '29.NOV.2019', endDate: '30.NOV.2019', type: 'company to company', file: 'file1.pdf', actions: 'delete' },
+  {position: 2, name: 'Contract 2', description: 'desc 2', startDate: '12.JUN.2019', endDate: '05.DEC.2019', type: 'company to employee', file: 'file2.pdf', actions: 'delete' },
+  {position: 3, name: 'Contract 3', description: 'desc 3', startDate: '13.JUN.2019', endDate: '03.DEC.2019', type: 'company to company', file: 'file3.pdf', actions: 'delete' },
+  {position: 4, name: 'Contract 4', description: 'desc 4', startDate: '14.JUN.2019', endDate: '08.DEC.2019', type: 'company to employee', file: 'file4.pdf', actions: 'delete' },
+  {position: 5, name: 'Contract 5', description: 'desc 5', startDate: '15.JUN.2019', endDate: '06.DEC.2019', type: 'company to employee', file: 'file5.pdf', actions: 'delete' },
+  {position: 6, name: 'Contract 6', description: 'desc 6', startDate: '16.JUN.2019', endDate: '09.DEC.2019', type: 'company to company', file: 'file6.pdf', actions: 'delete' },
 
 
 ];
@@ -37,7 +38,7 @@ export class ContractListComponent implements OnInit {
   // userSearch: string;
   // isAdmin$: Observable<boolean>;
 
-  displayedColumns: string[] = ['position', 'name', 'description', 'startDate', 'endDate', 'type', 'file'];
+  displayedColumns: string[] = ['position', 'name', 'description', 'startDate', 'endDate', 'type', 'file', 'actions'];
   dataSource = ELEMENT_DATA;
 
   constructor( private contractService: ContractService) {
